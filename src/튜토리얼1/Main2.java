6183272092c5e1aed0a6c5b63521ba7e9a4484f0
sjh0513 <kojo0513@naver.com>
2021-05-27 06:14:28 +0900

    두번째 commit 입니다

---
+++ src/튜토리얼1/Main2.java
@@ -0,0 +1,11 @@
+package 튜토리얼1;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("commit 연습겸 하나더");
+		//System.out.println("f11 버튼이 실행버튼이구나");
+	}
+
+}
