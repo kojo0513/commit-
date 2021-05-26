3379675de13db73f13c436fb7a4cc91d48004103
sjh0513 <kojo0513@naver.com>
2021-05-27 06:05:01 +0900

    this is commit

---
+++ src/튜토리얼1/Main.java
@@ -0,0 +1,11 @@
+package 튜토리얼1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 반갑다 첫 만남이야");
+		System.out.println("f11 버튼이 실행버튼이구나");
+	}
+
+}
